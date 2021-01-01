5c835050e7d63e8a0eb1d45e909e4b4451cdc603
same-js <tetravoxx@gmail.com>
2021-01-02 00:32:01 +0900

    ログイン API：ルート定義、コントローラー

--- web/app/Http/Controllers/Auth/LoginController.php
+++ web/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * `Illuminate\Foundation\Auth\AuthenticatesUsers@authenticated` をオーバーライド。
+     * ログイン成功後に実行すべき処理を実装する。
+     * @return [type] [description]
+     */
+    protected function authenticated (Request $request, $user) {
+        return $user;
     }
 }

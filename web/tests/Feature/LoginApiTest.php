7c4b50172e1a811a5b10558fbecb83ee13a8cc85
same-js <tetravoxx@gmail.com>
2021-01-02 00:29:12 +0900

    ログイン API：テストコード（テスト内容を実装）

--- web/tests/Feature/LoginApiTest.php
+++ web/tests/Feature/LoginApiTest.php
@@ -5,18 +5,40 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
 
 class LoginApiTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
-     *
+     * テスト開始前に実行すべき処理を実装する。
      * @return void
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    public function setUp():void {
+        parent::setUp();
+        // テストユーザの作成
+        $this->user = factory(User::class)->create();
+    }
 
-        $response->assertStatus(200);
+    /**
+     * ユーザのログイン要求成功時、下記を確認する。
+     * - レスポンスが正しいこと
+     * - ログイン要求ユーザで認証されていること
+     * @return void
+     */
+    public function test_login_user()
+    {
+        $response = $this-> json('POST', route('login'), [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ]);
+
+        // 検証1：レスポンスが正しいこと
+        $response
+            ->assertStatus(200)
+            ->assertJson(['name' => $this->user->name]);
+        // 検証2：ログイン要求ユーザで認証されていること
+        $this->assertAuthenticatedAs($this->user);
     }
 }

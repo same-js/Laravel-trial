4a67296b7da777aab836c4296bb4a3f574563f37
same-js <40536011+same-js@users.noreply.github.com>
2022-02-11 22:09:29 +0900

    [Try Passport] add route to generate PAT to route/api.php
    * PAT = Personal Access Token

--- Laravel9.x/www/routes/api.php
+++ Laravel9.x/www/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * パーソナルアクセストークンを発行し、そのトークンを json response にて返却する
+ * 発行に成功すると同時に、oauth_access_tokens テーブルに発行したトークンの情報が保存される
+ * User.php に HasApiTokens Trait を継承させたことにより、 createToken 関数が使用可能になっている
+ */
+Route::get('/publish-token/{id}', fn(string $id)
+    => response()->json(
+        [ 'token' => User::find($id)->createToken('Test Token')->accessToken ],
+    )
+);
